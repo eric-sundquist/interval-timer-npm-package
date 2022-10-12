27a2a69a4135cf555affaa8e5f6a52f18d70c08d
Eric Sundqvist <es225kz@student.lnu.se>
2022-10-12 16:05:13 +0200

    Setup dev environment

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello web app!</h1>
+    <div class="card">
+      <button id="counter" type="button">Mysterious button</button>
+    </div>
+    <p class="read-the-docs">
+      Click on the button for something exciting
+    </p>
+  </div>
+`
